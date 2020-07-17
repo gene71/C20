89e165f928e0de735be38b13ab6d8b3058ffadaf
geo <gene.moore@me>
2020-07-17 15:23:56 -0600

    updated init() to call functions that update local vectors by passing reference

--- mPUtil.cpp
+++ mPUtil.cpp
@@ -4,10 +4,7 @@
 #include <string>
 #include <fstream>
 
-using std::cout;
-using std::endl;
-using std::vector;
-using std::string;
+using namespace std;
 
 //cPFiles gets a vector of CPFile by using paths from getFiles_recursive
 vector<cPFile> cPFiles(std::vector<std::string> files)
@@ -28,7 +25,7 @@ vector<cPFile> cPFiles(std::vector<std::string> files)
         }
         cpf.lineCount = numLines;
         Pfiles.push_back(cpf);
-           
+
 
 
     }
@@ -46,76 +43,180 @@ vector<string> getFiles_recursive(const std::filesystem::path& path)
         if (!std::filesystem::is_directory(p))
         {
             files.push_back(p.path());
-            
-            
+
+
         }
     }
     return files;
 }
 
 //getCodeFile gets all the src files from the files vector and adds paths to the codeFile vector
-vector<string> getCodeFiles(vector<string> paths)
+// vector<string> getCodeFiles(vector<string> paths)
+// {
+//     vector<string> codeFiles;
+
+//     for(auto & p : paths)
+//     {
+//         if(p.substr(p.find_last_of(".") + 1) == "java")
+//         {
+//           codeFiles.push_back(p);
+
+//         }
+
+//         if(p.substr(p.find_last_of(".") + 1) == "cc")
+//         {
+//             codeFiles.push_back(p);
+
+//         }
+
+//         if(p.substr(p.find_last_of(".") + 1) == "c")
+//         {
+//             codeFiles.push_back(p);
+
+//         }
+
+//         if(p.substr(p.find_last_of(".") + 1) == "cpp")
+//         {
+//             codeFiles.push_back(p);
+
+//         }
+
+//         if(p.substr(p.find_last_of(".") + 1) == "h")
+//         {
+//             codeFiles.push_back(p);
+
+//         }
+
+//         if(p.substr(p.find_last_of(".") + 1) == "CC")
+//         {
+//             codeFiles.push_back(p);
+
+//         }
+
+//         if(p.substr(p.find_last_of(".") + 1) == "C")
+//         {
+//             codeFiles.push_back(p);
+
+//         }
+
+//         if(p.substr(p.find_last_of(".") + 1) == "CPP")
+//         {
+//             codeFiles.push_back(p);
+
+//         }
+
+//         if(p.substr(p.find_last_of(".") + 1) == "H")
+//         {
+//             codeFiles.push_back(p);
+
+//         }
+
+//     }//end for
+
+
+//     return codeFiles;
+// }
+
+// void saveScanConfig(string config, string name)
+// {
+//   // open a file in write mode.
+//    ofstream outfile;
+//    outfile.open(name + ".dat");
+//    // write inputted data into the file.
+//    outfile << config << endl;
+//
+// }
+
+// vector<string> getConfig(string configPath)
+// {
+//   string line;
+//   vector<string> v;
+//   // open a file in read mode.
+//   ifstream infile;
+//   infile.open(configPath);
+
+//   // again read the data from the file and display it.
+//   while(getline(infile, line))
+//   { //read data from file object and put it into string.
+//     v.push_back(line);
+//   }
+
+//   return v;
+
+// }
+
+// getConfigs ingests a file in key:value\n format and parses out value by removing key and delimeter from the line
+vector<string> getConfigs(string configPath, string key, string delimeter)
 {
-    vector<string> codeFiles;
+  // local vars
+  string line;
+  vector<string> v;
+  ifstream infile;
 
-    for(auto & p : paths)
+  // open a file in read mode.
+  infile.open(configPath);
+
+  // read the data from the file
+  while(getline(infile, line))
+  {
+    // parse key line
+    if(line.find(key) != std::string::npos)
     {
-        if(p.substr(p.find_last_of(".") + 1) == "java")
-        {
-            codeFiles.push_back(p);
+      size_t pos = 0;
+      string token;
+      //find position of delimeter to remove key and delimeter from line
+      while((pos = line.find(delimeter) != string::npos))
+      {
+        token = line.substr(0, pos);
+        // remove key and delimeter
+        line.erase(0, pos + delimeter.length());
+      }//end while pos
 
-        }
+      // add value to vector
+      v.push_back(line);
+    }//end if found key
 
-        if(p.substr(p.find_last_of(".") + 1) == "cc")
-        {
-            codeFiles.push_back(p);
+  }//end while getline
 
-        }
+  return v;
+}
+// setv_AllFiles reads the configs.dat  to get all files from scan directories and add the paths to v_allFiles vector
+void setv_AllFiles(string configsDat, string key, string delimeter, vector<string> &v)
+{
+  //cout << configsDat << " " << key << " " << delimeter << endl;
+  // read configs dat for scan directories and add file paths to allFiles std::vector<string> v;
+  for(auto & dir : getConfigs(configsDat, key, delimeter))
+  {
+    for(auto & f : getFiles_recursive(dir))
+    {
+            v.push_back(f);
+    }
+  }//end get all files
+}
+//setv_codeExt reads the configs.dat to get all code file extenstions and adds them to the v_codeExt vector
+void setv_codeExt(string configsDat, string key, string delimeter, vector<string> &v)
+{
+  //cout << configsDat << " " << key << " " << delimeter << endl;
+  // read configs dat for scan directories and add file paths to allFiles std::vector<string> v;
+  for(auto & ext : getConfigs(configsDat, key, delimeter))
+  {
+    v.push_back(ext);    
+  }//end get all files
+  
+}
+//setv_filesToScan searches the v_allFiles vector using the file extensions in the v_codeExt vector to find and add code file paths to the v_filesToScanVector
+void setv_filesToScan(vector<string> &v_aF, vector<string> &v_cFE, vector<string> &v_fTS)
+{
+  for(auto & f : v_aF)
+  {
+    for(auto & ext : v_cFE)
+    {
+      if(f.substr(f.find_last_of(".") + 1) == ext)
+      {
+        v_fTS.push_back(f);
 
-        if(p.substr(p.find_last_of(".") + 1) == "c")
-        {
-            codeFiles.push_back(p);
-
-        }
-
-        if(p.substr(p.find_last_of(".") + 1) == "cpp")
-        {
-            codeFiles.push_back(p);
-
-        }
-
-        if(p.substr(p.find_last_of(".") + 1) == "h")
-        {
-            codeFiles.push_back(p);
-
-        }
-
-        if(p.substr(p.find_last_of(".") + 1) == "CC")
-        {
-            codeFiles.push_back(p);
-
-        }
-        
-        if(p.substr(p.find_last_of(".") + 1) == "C")
-        {
-            codeFiles.push_back(p);
-
-        }
-
-        if(p.substr(p.find_last_of(".") + 1) == "CPP")
-        {
-            codeFiles.push_back(p);
-
-        }
-
-        if(p.substr(p.find_last_of(".") + 1) == "H")
-        {
-            codeFiles.push_back(p);
-
-        }
-
-    }//end for
-
-
-    return codeFiles;
+      }
+    }//end for ext
+  }
+    
 }
--- test.cpp
+++ test.cpp
@@ -10,35 +10,74 @@ int testCPFile();
 void testCPFiles();
 void testGetRecurseFile();
 void testGetCodeFiles();
+void testSaveScanConfig();
+void testGetTestDir();
+void testInit();
+void init();
+void testGetConfigs();
+void testSetV();
 
 using std::cout;
 using std::endl;
 using std::vector;
 using std::string;
 
+vector<string> v_allFiles;
+vector<string> v_codeFileExt;
+vector<string> v_filesToScan;
+vector<string> config;
+
+string configsDat = "/home/geo/source/parascan/dat/configs.dat";
+string delimeter = ":";
+
 //main***********************************************************************************//
-int main() 
+int main()
 {
+   init();
+   testInit();
+  //testSetV();
+  //testGetConfigs();
     //testCPFile();
     //testCPFiles();
     //testGetRecurseFile();
-    testGetCodeFiles();  
+    //testGetCodeFiles();
+    //testSaveScanConfig();
+    //testGetTestDir();
+
     return 0;
 }
+
+void init()
+{
+
+  // read configs dat for scan directories and add file paths to allFiles std::vector<string> v;
+  setv_AllFiles(configsDat, "scanDir", delimeter, v_allFiles);
+  
+  // get code file extensions from configs dat and add them to
+  setv_codeExt(configsDat, "codeExt", delimeter, v_codeFileExt);
+  
+  // get code file paths from all files and add to files filesToScan std::vector<string> v;
+  setv_filesToScan(v_allFiles, v_codeFileExt, v_filesToScan);
+  
+
+  return;
+
+
+}//end init()
 
 int testCPFile()
 {
     cPFile myObj;  // Create an object of MyClass
 
     // Access attributes and set values
-    myObj.lineCount = 15; 
+    myObj.lineCount = 15;
     myObj.filePath = "Some path";
 
     // Print attribute values
     cout << myObj.lineCount << endl;
     cout << myObj.filePath << endl;
     return 0;
-   
+
 }
 
 //this test also tests getFiles_recursive
@@ -66,14 +105,81 @@ void testGetRecurseFile()
     return;
 }
 
-void testGetCodeFiles()
+// void testGetCodeFiles()
+// {
+//     vector<string> codeFiles = getCodeFiles(getFiles_recursive("/home/geo/source/parascan"));
+
+//     for(auto & cf : codeFiles)
+//     {
+//         cout << cf << endl;
+//     }
+
+
+// }
+
+void testSaveScanConfig()
 {
-    vector<string> codeFiles = getCodeFiles(getFiles_recursive("/home/geo/source/parascan"));
+  string name = "testFile";
+  string config = "LKJLKJLKJLKJLKJLKJJ<<<<<>>>>>>>";
+  //saveScanConfig(config, name);
 
-    for(auto & cf : codeFiles)
-    {
-        cout << cf << endl;
-    }
+}
 
-    
+// void testGetTestDir()
+// {
+//   std::vector<string> v = getConfig("/home/geo/source/parascan/dat/config.dat");
+//   for(auto & configs : v)
+//   {
+//     std::cout << configs << endl;
+//   }
+
+// }
+
+void testInit()
+{
+
+  for(auto & dir : v_allFiles)
+  {
+    cout << "directories: " << dir << endl;
+  }
+
+  for(auto & ext : v_codeFileExt)
+  {
+    cout << "extensions to scan: " << ext << endl;
+  }
+
+  for(auto & f : v_filesToScan)
+  {
+    cout << "file to scan: " << f << endl;
+  }
+
+}
+
+void testGetConfigs()
+{
+  string configPath = "/home/geo/source/parascan/dat/configs.dat";
+  string key = "codeExt";
+  string delimeter = ":";
+  vector<string> v = getConfigs(configPath, key, delimeter);
+  for(auto & c : v)
+  {
+    cout << c << endl;
+  }
+  return;
+}
+
+void testSetV()
+{
+  setv_AllFiles(configsDat, "scanDir", delimeter, v_allFiles);
+  //setV(configsDat, "codeExt", delimeter, v_codeFileExt);
+  for(auto & f : v_allFiles)
+  {
+    cout << f << endl;
+  }
+
+  // for(auto & ext : v_codeFileExt)
+  // {
+  //   cout << ext << endl;
+  // }
+
 }
